a07beb40ad4536dd5f44a02621b1cdf5ed2b2b02
Nate <hello@naterichardson.com>
2026-03-24 17:28:54 -0400

    sick of the blog and x

--- src/data/social.ts
+++ src/data/social.ts
@@ -20,11 +20,6 @@ export const socialLinks: SocialLink[] = [
     url: 'https://bsky.app/profile/naterichardson.com',
     icon: 'bluesky',
   },
-  {
-    label: 'X',
-    url: 'https://x.com/naterchrdsn',
-    icon: 'x',
-  },
 ];
 
 export const contactInfo = {
